ecc4e7bcc87834bdf1cdff998aab30fb30264181
mahnoor adnan <mahnooradnan479@gmail.com>
2026-01-01 17:10:50 +0500

    Prepare frontend for Netlify deployment

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: true,
+    // appDir: true,
   },
   env: {
     NEXT_PUBLIC_BETTER_AUTH_URL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL,
