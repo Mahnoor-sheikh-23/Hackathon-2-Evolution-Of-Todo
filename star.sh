35d4b9e9cb8230c699121873e2a8d70b5934e4bc
mahnoor adnan <mahnooradnan479@gmail.com>
2026-01-01 19:09:19 +0500

    move erquiremnts again in backend

--- star.sh
+++ star.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-uvicorn backend.main:app --host 0.0.0.0 --port $PORT
+# Move into backend folder
+cd backend
 
+# Upgrade pip and install dependencies
+pip install --upgrade pip
+pip install -r requirements.txt
+
+# Start the FastAPI app
+uvicorn main:app --host 0.0.0.0 --port $PORT
