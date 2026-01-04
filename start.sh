3f5117ae7117c7de7f61660757970a1ad308d4e6
mahnoor adnan <mahnooradnan479@gmail.com>
2026-01-04 18:56:05 +0500

    Fix Railway start command

--- start.sh
+++ start.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-
-# Go to backend folder
 set -e
+
 cd backend
 
-# Install dependencies in the Railway environment
-
-
-# Start the FastAPI app
-uvicorn main:app --host 0.0.0.0 --port ${PORT:-8000} --reload
+exec uvicorn main:app \
+  --host 0.0.0.0 \
+  --port ${PORT}
